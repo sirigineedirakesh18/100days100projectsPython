ccdea6d4cd9329ee6abe8bcd2273dfb191e5d8f1
Rakesh Mourya <sirigineedirakesh@gmail.com>
2025-05-12 12:20:27 +0530

    Add files via upload

---
+++ day_6_goal_reached.py
@@ -0,0 +1,11 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+while not at_goal:
+    if right_is_clear():
+        turn_right():
+    elif front_is_clear():
+        move():
+    else:
+        turn_left()
